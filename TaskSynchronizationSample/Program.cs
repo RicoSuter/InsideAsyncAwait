b9b764b63d0110ae8a864aa37b819dea4d7d29ff
Rico Suter <mail@rsuter.com>
2015-11-10 18:35:20 +0100

    Improved sample

--- TaskSynchronizationSample/Program.cs
+++ TaskSynchronizationSample/Program.cs
@@ -43,37 +43,37 @@ namespace TaskSynchronizationSample
     // https://github.com/MyToolkit/MyToolkit/blob/master/src/MyToolkit/Utilities/TaskSynchronizationScope.cs
 
     /// <summary>Synchronizes tasks so that they are executed after each other.</summary>
-    public class TaskSynchronizationScope : TaskSynchronizationScope<object>
-    {
-        /// <summary>Executes the given task when the previous task has been completed.</summary>
-        /// <param name="task">The task function.</param>
-        /// <returns>The task.</returns>
-        public Task RunAsync(Func<Task> task)
-        {
-            return RunAsync(async () =>
-            {
-                await task();
-                return null;
-            });
-        }
-    }
-
-    /// <summary>Synchronizes tasks so that they are executed after each other.</summary>
     /// <typeparam name="T">The return type of the task.</typeparam>
-    public class TaskSynchronizationScope<T>
+    public class TaskSynchronizationScope
     {
-        private Task<T> _currentTask;
+        private Task _currentTask;
         private readonly object _lock = new object();
 
         /// <summary>Executes the given task when the previous task has been completed.</summary>
         /// <param name="task">The task function.</param>
         /// <returns>The task.</returns>
-        public Task<T> RunAsync(Func<Task<T>> task)
+        public Task RunAsync(Func<Task> task)
+        {
+            return RunAsync<object>(async () =>
+            {
+                await task();
+                return null;
+            });
+        }
+
+        /// <summary>Executes the given task when the previous task has been completed.</summary>
+        /// <param name="task">The task function.</param>
+        /// <returns>The task.</returns>
+        public Task<T> RunAsync<T>(Func<Task<T>> task)
         {
             lock (_lock)
             {
                 if (_currentTask == null)
-                    _currentTask = task();
+                {
+                    var currentTask = task();
+                    _currentTask = currentTask;
+                    return currentTask;
+                }
                 else
                 {
                     var source = new TaskCompletionSource<T>();
@@ -82,10 +82,10 @@ namespace TaskSynchronizationSample
                         var nextTask = task();
                         nextTask.ContinueWith(nt =>
                         {
-                            if (t.IsCompleted)
-                                source.SetResult(t.Result);
-                            else if (t.IsFaulted)
-                                source.SetException(t.Exception);
+                            if (nt.IsCompleted)
+                                source.SetResult(nt.Result);
+                            else if (nt.IsFaulted)
+                                source.SetException(nt.Exception);
                             else
                                 source.SetCanceled();
 
@@ -97,8 +97,8 @@ namespace TaskSynchronizationSample
                         });
                     });
                     _currentTask = source.Task;
+                    return source.Task;
                 }
-                return _currentTask;
             }
         }
     }
